33222ad8de26f51fbfcdec6f4f7a6ed71116661f
riya-29 <60510052+riya-29@users.noreply.github.com>
2021-07-26 01:00:59 +0530

    Add files via upload

---
+++ Riya/Speech_to_text.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 17:37:46 2021
+
+@author: riya
+"""
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+# Reading Audio file as source
+# listening the audio file and store in audio_text variable
+
+with sr.AudioFile(r'D:/aud.wav') as source:
+    audio_text = r.listen(source)
+    try:
+        # using google speech recognition
+        text = r.recognize_google(audio_text)
+        print('Converting Audio to Text...')
+        print(text)
+     
+    except:
+         print('Try again...')
